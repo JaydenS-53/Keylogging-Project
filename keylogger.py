b3b5a52c20f6b9cacd0b4650d8d3650438a458da
Jayden Sleeuwenhoek <sleeuwenhj@coventry.ac.uk>
2023-07-10 15:47:38 +0100

    created a key scrambler that encrypts keystrokes using the fernet technique in the cryptography library

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,2 @@
-print("test")
+from pynput import keyboard
+
---
+++ keyscrambler.py
@@ -0,0 +1,36 @@
+from pynput import keyboard
+from cryptography.fernet import Fernet
+
+# Generate a random encryption key
+key = Fernet.generate_key()
+# Create an instance of the Fernet cipher using the encryption key
+cipher = Fernet(key)
+
+
+def encrypt_keystrokes(key):
+    # Check if the key is a printable character
+    if hasattr(key, 'char'):
+        char = key.char
+        # Convert the character to bytes
+        plaintext = char.encode()
+        # Encrypt the keystroke using the Fernet cipher
+        encrypted_keystroke = cipher.encrypt(plaintext)
+        # Print the encrypted keystroke
+        print(encrypted_keystroke.decode(), end='\n', flush=True)
+
+
+def on_press(key):
+    # Encrypt the keystroke
+    encrypt_keystrokes(key)
+
+
+def on_release(key):
+    # Stop the listener when the Escape key is pressed
+    if key == keyboard.Key.esc:
+        return False
+
+
+# Start the listener
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    print("Encrypting keystrokes. Press ESC to exit.")
+    listener.join()
